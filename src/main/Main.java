9d43e18d29d90aa067816d293aaac6b235aaecb0
Jhojan <pipeflx85@gmail.com>
2020-02-07 18:59:55 -0500

    Jena

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,26 @@ package main;
 
 import javax.swing.*;
 
+// Librerías de Java
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+// Librerías de Jena
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.util.FileManager;
+
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
 
+        // GUI
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
@@ -19,6 +35,13 @@ public class Main {
                 String[] instances = {"libro", "autor"};
             }
         });
+
+        // Create Model
+        // Model data = FileManager.get().loadModel("ModelIntegration.owl");
+        // InfModel infmodel = ModelFactory.createRDFSModel(data);
+
+        // Entities
+
 
     }
 
