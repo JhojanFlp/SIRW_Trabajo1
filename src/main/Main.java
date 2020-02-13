1ef17e25b8b42131ede77f29c8306797b6d77c41
Victor Cataño <vdcatanoc@unal.edu.co>
2020-02-12 19:31:31 -0500

    implementada la consulta en varios endpoints con la integracion

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,22 +8,28 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 // Librerías de Jena
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.reasoner.ValidityReport;
 import org.apache.jena.util.FileManager;
 
 public class Main {
+    public static InfModel model;
 
     public static void main(String[] args) throws FileNotFoundException{
 
         // GUI
-        /*SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 JFrame frame = new Panel();
@@ -35,18 +41,18 @@ public class Main {
                 //Fill to CB Instance
                 String[] instances = {"libro", "autor"};
             }
-        });*/
+        });
 
         // Create Model
-        Model model = ModelFactory.createDefaultModel() ;
-        model.read("Libros.ttl") ;
+        Model model1 = ModelFactory.createDefaultModel() ;
+        model1.read("Libros.ttl") ;
 
         //razonador
         Reasoner OWLReasoner = ReasonerRegistry.getOWLReasoner();
-        InfModel OWLinfe = ModelFactory.createInfModel(OWLReasoner,model);
+        model = ModelFactory.createInfModel(OWLReasoner,model1);
 
         //validation
-        ValidityReport validity = OWLinfe.validate();
+        ValidityReport validity = model.validate();
         if (validity.isValid()) {
             System.out.println("OK");
         } else {
@@ -56,54 +62,126 @@ public class Main {
             }
         }
 
-        //consulta sparql
-        String queryString = "PREFIX book: <http://book.org/>\n" +
-                "PREFIX book1: <http://book1.org/#>\n" +
-                "\n" +
-                "SELECT DISTINCT * \n" +
-                "\n" +
-                "WHERE {\n" +
-                "\t{\n" +
-                "\tbook:Ebay ?p ?o .\n" +
+        //ejemplo de consulta
+        LinkedList<HashMap<String, String>> a = consultaEnTodasLasBD("prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "prefix book: <http://book.org/> \n" +
+                "prefix book1: <http://book1.org/#>\n" +
+                "SELECT DISTINCT ?a ?b WHERE {\n" +
+                "  ?a book:Distribuido_Por ?b\n" +
                 "}\n" +
-                "UNION\n" +
-                "{\n" +
-                "\tSERVICE <http://35.188.55.150:8890/sparql?default-graph-uri=http%3A%2F%2Fbook.org%2F>{\n" +
-                "  \t\tbook:Ebay ?p ?o .\n" +
-                "\t}\n" +
-                "}\n" +
-                "\n" +
-                "}\n" +
-                "LIMIT 100";
-        Query query = QueryFactory.create(queryString);
-        QueryExecution qexec = QueryExecutionFactory.create(query,model);
-
-        try{
-            ResultSet results = qexec.execSelect();
-            while (results.hasNext()){
-                QuerySolution soln = results.nextSolution();
-
-                /*Literal s = soln.getLiteral("s");
-                Literal p = soln.getLiteral("p");
-                Literal o = soln.getLiteral("o");
-                System.out.println(s+" "+p+" "+o);*/
-                System.out.println(soln.toString());
-            }
-        } finally {
-            qexec.close();
+                "LIMIT 100",new String[]{"a","b"});
+        for(HashMap r:a){
+            System.out.println("a: "+r.get("a")+"b: "+r.get("b"));
         }
-
-        //OWLinfe.write(System.out);
-
-        // Print Tripletas
-        // for(StmtIterator i = data.listStatements(); i.hasNext();) {
-        //     Statement statement = i.nextStatement();
-        //    System.out.println(statement.getSubject() + " " + statement.getPredicate() + " " + statement.getObject());
-        // }
-
-        // Entities
 
 
     }
+    public static String nombreEquivalenteEnLaOntologia(String nombre, String nuevaIri){
+        String queryString = String.format("prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "prefix book: <http://book.org/> \n" +
+                "prefix book1: <http://book1.org/#>\n" +
+                "select ?r\n" +
+                "where {\n" +
+                "{?r owl:equivalentClass %1$s}\n" +
+                "union\n" +
+                "{?r owl:equivalentProperty %1$s}\n" +
+                "filter strstarts(str(?r),str(%2$s))\n" +
+                "}",nombre,nuevaIri);
+        //System.out.println(queryString);
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.create(query,model);
+        String r ="error";
+        try {
+
+            ResultSet results = qexec.execSelect();
+
+            while (results.hasNext()){
+                QuerySolution soln = results.nextSolution();
+                r = soln.get("r").toString();
+            }
+
+        } finally {
+            qexec.close();
+
+        }
+        return r;
+    }
+    public static String traducirConsultaA(String consulta, String prefijoOriginal, String nuevaIRI){
+        String[] consultaPartida = consulta.split(" ");
+        for (int i =0; i<consultaPartida.length;i++) { //asegurarse que tododos los books:ID tengan espacios alrededor
+            if(consultaPartida[i].matches("^"+prefijoOriginal+"\\S+")){
+                consultaPartida[i] = "<"+nombreEquivalenteEnLaOntologia(consultaPartida[i],nuevaIRI)+">";
+            }
+        }
+        return String.join(" ", consultaPartida);
+    }
+
+    public static LinkedList<HashMap<String,String>> consultaEnTodasLasBD(
+            String consulta, String[] variables){
+        LinkedList res = new LinkedList();
+        /*consultaEnEndpoint(
+                traducirConsultaA(consulta,"book:","book1:"),
+                variables,
+                "");*/
+
+        res.addAll(
+                consultaEnEndpoint(
+                consulta, variables, "http://35.188.55.150:8890/sparql"
+                ));
+        res.addAll(
+                consultaEnRDF(
+                        traducirConsultaA(consulta,"book:","book1:"),
+                        variables
+                ));
+        return res;
+
+
+    };
+    public static LinkedList<HashMap<String,String>> consultaEnEndpoint(
+            String consulta, String[] variables, String endpoint
+    ){
+        LinkedList res = new LinkedList();
+        RDFConnection conn = RDFConnectionFactory.connect(endpoint);
+
+        QueryExecution qExec = conn.query(consulta) ;
+        ResultSet rs = qExec.execSelect() ;
+        while(rs.hasNext()) {
+            QuerySolution qs = rs.next() ;
+            HashMap fila = new HashMap();
+            for(String v: variables){
+                fila.put(v,qs.get(v).toString());
+            }
+            res.add(fila);
+        }
+        qExec.close() ;
+        conn.close() ;
+        return res;
+    }
+    public static LinkedList<HashMap<String,String>> consultaEnRDF(
+            String consulta, String[] variables
+    ){
+        System.out.println("/////////////////////\n"+consulta);
+        Model modelRDF = ModelFactory.createDefaultModel() ;
+        modelRDF.read("RDF1.xml") ;
+
+        LinkedList res = new LinkedList();
+        Query query = QueryFactory.create(consulta);
+        QueryExecution qExec = QueryExecutionFactory.create(query,modelRDF);
+
+        try{
+            ResultSet rs = qExec.execSelect() ;
+            while(rs.hasNext()) {
+                QuerySolution qs = rs.next() ;
+                HashMap fila = new HashMap();
+                for(String v: variables){
+                    fila.put(v,qs.get(v).toString());
+                }
+                res.add(fila);
+            }
+        } finally {
+            qExec.close() ;
+            return res;
+        }
+    };
 
 }
