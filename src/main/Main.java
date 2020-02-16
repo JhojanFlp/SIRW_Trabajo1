1a032d2cad0a0d77524f3de343b766f67bc4dcbe
Victor Cataño <vdcatanoc@unal.edu.co>
2020-02-15 21:12:13 -0500

    Terminada la consulta 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -140,11 +140,11 @@ public class Main {
                         traducirConsultaA(consulta,"book:","book1:"),
                         variables
                 ));
-        res.addAll(consultaEnEndpoint(
+        /*res.addAll(consultaEnEndpoint(
                 traducirConsultaA(consulta,"book:","dbo:")+"\nlimit 15",
                 variables,
                 "http://dbpedia.org/sparql/"
-        ));
+        ));*/
         return res;
 
 
--- src/main/Panel2.java
+++ src/main/Panel2.java
@@ -1,4 +1,132 @@
 package main;
 
-public class Panel2 {
+import javax.swing.*;
+import java.awt.event.*;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import static main.Main.consultaEnTodasLasBD;
+import static main.Main.prefijos;
+
+public class Panel2 extends JFrame {
+    private JPanel Panel2;
+    private JComboBox entityCB;
+    private JComboBox instanceCB;
+    private JComboBox featureCB;
+    private JTextArea textArea;
+    private JButton buscarButton;
+
+
+    public Panel2() {
+
+        super("SIRW - Trabajo 1 (Consulta #2)");
+        setContentPane(Panel2);
+
+        // Get values and fill ComboBox
+        String consulta = prefijos + "select distinct ?class \n" +
+                "where {\n" +
+                "\t{?a a ?class.\n" +
+                "\t?class a owl:Class.}\n" +
+                "union {\n" +
+                "\t?aux a owl:Class.\n" +
+                "\t?class rdfs:subClassOf ?aux. }" +
+                "}\n";
+
+        String consultaF = prefijos + "select distinct ?property \n" +
+                "where {\n" +
+                "\t?a ?property ?b.\n" +
+                "\t?property a owl:DatatypeProperty.\n" +
+                "}\n";
+
+
+        LinkedList<HashMap<String, String>> a = consultaEnTodasLasBD(consulta, new String[]{"class"});
+        //LinkedList<HashMap<String, String>> b = consultaEnTodasLasBD(consultaF, new String[]{"property"});
+
+        for (HashMap r : a) {
+            this.entityCB.addItem(r.get("class"));
+        }
+
+
+        // Accion a realizar cuando el JComboBox cambia de item seleccionado.
+        entityCB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String entidadSeleccionada = entityCB.getSelectedItem().toString();
+                String consulta = prefijos + "select distinct ?instance\n" +
+                        "where{\n" +
+                        "\t?instance a <" + entidadSeleccionada + ">\n" +
+                        "}";
+                LinkedList<HashMap<String, String>> a = consultaEnTodasLasBD(consulta, new String[]{"instance"});
+                instanceCB.removeAllItems();
+                for (HashMap r : a) {
+                    instanceCB.addItem(r.get("instance"));
+                }
+            }
+        });
+
+        instanceCB.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String instanciaSeleccionada = instanceCB.getSelectedItem().toString();
+                String consulta = prefijos + "select distinct ?property\n" +
+                        "where{\n" +
+                        "\t<" + instanciaSeleccionada + "> ?property ?b.\n" +
+                        "\t?property a owl:DatatypeProperty.\n" +
+                        "}";
+                LinkedList<HashMap<String, String>> a = consultaEnTodasLasBD(consulta, new String[]{"property"});
+                featureCB.removeAllItems();
+                for (HashMap r : a) {
+                    featureCB.addItem(r.get("property"));
+                }
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        buscarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String entidad = entityCB.getSelectedItem().toString();
+                String instancia = instanceCB.getSelectedItem().toString();
+                String atributo = featureCB.getSelectedItem().toString();
+
+                String res = "";
+                String consulta = prefijos + "select distinct ?r\n" +
+                        "where{\n" +
+                        "\t?r a <"+entidad+">;\n" +
+                        "\t\t<"+atributo+"> ?atr.\n" +
+                        "\t\t<"+instancia+"> <"+atributo+"> ?atr.\n" +
+                        "filter(?r != <"+instancia+">)." +
+                        "}";
+                LinkedList<HashMap<String, String>> a = consultaEnTodasLasBD(consulta, new String[]{"r"});
+                for (HashMap r : a) {
+                    res = res + r.get("r");
+                }
+
+                textArea.setText(res);
+
+            }
+        });
+
+
+    }
 }
